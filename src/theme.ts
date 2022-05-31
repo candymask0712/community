8b1ab85fa45bb032f728c3cdc06b985153657d0f
Roger <candymask0712@gmail.com>
2022-06-01 02:23:13 +0900

    Style: 전역 스타일 관리를 위한 파일 추가

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    bgColor: '#f5f6fa',
+    accentColor: '#358BCA',
+    darkGrey: `#B8B8B8`
+  }
+};
