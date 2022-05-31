1990ec4cbde8757ca12b36f38d39f82507cf79f1
Roger <candymask0712@gmail.com>
2022-06-01 02:33:38 +0900

    Feat: 전체 페이지 라우팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from './pages/Main';
+import AddPost from './pages/AddPost';
+import List from './pages/List';
+import NotFound from './pages/NotFound';
+import PostDetail from './pages/PostDetail';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />}></Route>
+        <Route path='/' element={<List />}></Route>
+        <Route path='/community/list' element={<List />}></Route>
+        <Route path='/community/post/:post_pk' element={<PostDetail />}></Route>
+        <Route path='/community/post/new' element={<AddPost />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
