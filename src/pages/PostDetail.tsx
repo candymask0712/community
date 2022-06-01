f9a7a252b27c0a4b8ae494398b8b506632e464ce
Roger <candymask0712@gmail.com>
2022-06-01 18:51:39 +0900

    Feat: 상세 페이지 내 좋아요 페이크 기능 구현(서버 통신은 미 연결)

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -6,6 +6,8 @@ import Profile from '../components/common/Profile/Profile';
 
 const PostDetail = () => {
   const path = window.location.pathname.split('/')[3];
+  const [countForLike, setCountForLike] = useState(0);
+  const [likeStatus, setLikeStatus] = useState(false);
 
   const [postData, setPostData] = useState<DataType>({
     categoryPk: 0,
@@ -28,6 +30,7 @@ const PostDetail = () => {
       method: 'get' // 통신할 방식
     }).then((reponse) => {
       setPostData(reponse.data);
+      setCountForLike(reponse.data.likeCount);
     });
   }, []);
 
@@ -56,6 +59,12 @@ const PostDetail = () => {
     );
   }
 
+  const contorlLike = () => {
+    if (likeStatus) setCountForLike(countForLike - 1);
+    else setCountForLike(countForLike + 1);
+    setLikeStatus(!likeStatus);
+  };
+
   return (
     <Container>
       <Profile data={postData} />
@@ -70,6 +79,20 @@ const PostDetail = () => {
           <Img src={postData.imageUrl} />
         )
       ) : null}
+      <LikeAndCommentContainer>
+        <GreyContainer onClick={contorlLike}>
+          <Icon
+            src={`${process.env.PUBLIC_URL}/img/like-${
+              likeStatus ? 'after' : 'before'
+            }.png`}
+          />
+          <Count>{countForLike}</Count>
+        </GreyContainer>
+        <GreyContainer>
+          <Icon src={`${process.env.PUBLIC_URL}/img/comment.png`} />
+          <Count>{postData.commentCount}</Count>
+        </GreyContainer>
+      </LikeAndCommentContainer>
     </Container>
   );
 };
@@ -102,4 +125,28 @@ const Img = styled.img`
   margin: 10px 0px 0px 0px;
   width: 100%;
   object-fit: cover;
+`;
+
+const LikeAndCommentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 15px 0px 15px 10px;
+`;
+const GreyContainer = styled.div`
+  display: flex;
+  background-color: #f8f8f8;
+  margin: 0px 0px 0px 15px;
+  padding: 9px;
+`;
+const Icon = styled.img`
+  width: 14px;
+  object-fit: contain;
+`;
+const Count = styled.div`
+  font-family: 'Noto Sans KR';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  color: #7a7a7a;
+  margin: 0px 0px 0px 7px;
 `;
